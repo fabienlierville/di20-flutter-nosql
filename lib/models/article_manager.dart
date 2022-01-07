c052e0ae3238d883fa8843127ad3843311b8d086
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-07 14:42:36 +0100

    Model Article

---
+++ lib/models/article_manager.dart
@@ -0,0 +1,20 @@
+
+import 'dart:async';
+
+import 'package:courses/global_vars.dart';
+import 'package:courses/models/article.dart';
+import 'package:courses/models/magasin.dart';
+import 'package:courses/objectbox.g.dart';
+
+class ArticleManager{
+
+  static Stream<List<Article>> getAllStream(Magasin mag){
+    StreamController<List<Article>> controller = StreamController<List<Article>>();
+    if(GlobalVars.store != null){
+      Stream<List<Article>> articles = GlobalVars.store!.box<Article>().query(Article_.magasin.equals(mag.id)).watch(triggerImmediately: true).map((event) => event.find());
+      controller.addStream(articles);
+    }
+    return controller.stream;
+  }
+
+}
