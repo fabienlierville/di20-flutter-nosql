3f94777a83f2afe7f1c6664da737ffebf0a027f5
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-07 09:15:34 +0100

    init

---
+++ lib/pages/page_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageHome extends StatefulWidget {
+  const PageHome({Key? key}) : super(key: key);
+
+  @override
+  _PageHomeState createState() => _PageHomeState();
+}
+
+class _PageHomeState extends State<PageHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
